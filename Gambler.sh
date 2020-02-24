f624f9d4eca0f4f6bbdf821c6ada1d45b6244a68
Shakti <shaktiprasadsatapathy96@gmail.com>
2020-02-24 16:39:01 +0530

    Added functionality of play for 20 days in a month

--- Gambler.sh
+++ Gambler.sh
@@ -1,24 +1,28 @@
 #!/bin/bash -x
 
-stack=100	#starting amount
 win=0	# initialing win
 loose=0	# initialing loose
-
-while [[ $stack -gt 50 && $stack -lt 150 ]]
+for (( i = 0; i < 20; i++ ))
 do
-	bet=$((RANDOM%2))
+	stack=100   #starting amount
+	while [[ $stack -gt 50 && $stack -lt 150 ]]
+	do
+		bet=$((RANDOM%2))
 
-	if [[ $bet -eq 1 ]]
-	then
-		stack=$(( stack + 1 ))
-	else
-		stack=$(( stack - 1 ))	
-	fi
-	if [[ $stack -eq 150 ]]
-   then
-      win=$(( win + 1 ))
-   elif [[ $stack -eq 50 ]]
-	then
-      loose=$(( loose + 1 ))  
-   fi
+		if [[ $bet -eq 1 ]]
+		then
+			stack=$(( stack + 1 ))
+		else
+			stack=$(( stack - 1 ))	
+		fi
+		if [[ $stack -eq 150 ]]
+   	then
+      	win=$(( win + 1 ))
+   	elif [[ $stack -eq 50 ]]
+		then
+      	loose=$(( loose + 1 ))  
+   	fi
+	done
 done
+
+echo $win oooooooo $loose ooooooooo $stack
