18df0b4646c9b5fbd4c938cc5a5e494122d28716
Shakti <shaktiprasadsatapathy96@gmail.com>
2020-02-24 16:21:06 +0530

    added betting and reflection on stack

--- Gambler.sh
+++ Gambler.sh
@@ -4,4 +4,12 @@ stack=100	#starting amount
 win=0	# initialing win
 loose=0	# initialing loose
 
+bet=$((RANDOM%2))
 
+if [[ $bet -eq 1 ]]
+then
+	stack=$(( stack + 1 ))
+else
+	stack=$(( stack - 1 ))
+fi
+echo $stack
