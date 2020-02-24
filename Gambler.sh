d24973f828ad77ece0e1297da48217d7caf4dd3b
Shakti <shaktiprasadsatapathy96@gmail.com>
2020-02-24 16:31:50 +0530

    added limit to play per day ie. if stack became 150 or 50 then quit the game

--- Gambler.sh
+++ Gambler.sh
@@ -4,12 +4,21 @@ stack=100	#starting amount
 win=0	# initialing win
 loose=0	# initialing loose
 
-bet=$((RANDOM%2))
+while [[ $stack -gt 50 && $stack -lt 150 ]]
+do
+	bet=$((RANDOM%2))
 
-if [[ $bet -eq 1 ]]
-then
-	stack=$(( stack + 1 ))
-else
-	stack=$(( stack - 1 ))
-fi
-echo $stack
+	if [[ $bet -eq 1 ]]
+	then
+		stack=$(( stack + 1 ))
+	else
+		stack=$(( stack - 1 ))	
+	fi
+	if [[ $stack -eq 150 ]]
+   then
+      win=$(( win + 1 ))
+   elif [[ $stack -eq 50 ]]
+	then
+      loose=$(( loose + 1 ))  
+   fi
+done
